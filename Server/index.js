2a3ecf484d3cc1559eb29c66ae287db83e7c562f
pauldumitriu <paul.dumitriu01@gmail.com>
2024-01-17 16:32:45 -0500

    Root push

---
+++ Server/index.js
@@ -0,0 +1,163 @@
+import express from "express";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3500;
+const ADMIN = "Admin";
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+// state
+const UsersState = {
+  users: [],
+  setUsers: function (newUsersArray) {
+    this.users = newUsersArray;
+  },
+};
+
+const io = new Server(expressServer, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production"
+        ? false
+        : ["http://localhost:5500", "http://127.0.0.1:5500"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} connected`);
+
+  //upon connection - to user that connected
+  socket.emit("message", buildMessage(ADMIN, "Welcome to Chat App!"));
+
+  socket.on("enterRoom", ({ name, room }) => {
+    //leave a previous room
+    const previousRoom = getUser(socket.id)?.room;
+    if (previousRoom) {
+      socket.leave(previousRoom);
+      io.to(previousRoom).emit(
+        "message",
+        buildMessage(ADMIN, `${name} has left the room`)
+      );
+    }
+
+    const user = activateUser(socket.id, name, room);
+
+    //can't update prev room users until we update the userState
+    if (previousRoom) {
+      io.to().emit("userList", {
+        users: getUsersInRoom(previousRoom),
+      });
+    }
+
+    //join room
+    socket.join(user.room);
+
+    // to user who joined
+    socket.emit(
+      "message",
+      buildMessage(ADMIN, `You have joined room ${user.room}`)
+    );
+
+    // To everyone else
+    socket.broadcast
+      .to(user.room)
+      .emit("message", buildMessage(ADMIN, `${user.name} has joined the room`));
+
+    // Update user list for room
+    io.to(user.room).emit("userList", {
+      users: getUsersInRoom(user.room),
+    });
+
+    // Update rooms list for everyone
+    io.emit("roomList", {
+      rooms: getAllActiveRooms(),
+    });
+  });
+
+  //when user dissconnects
+  socket.on("disconnect", () => {
+    const getUser = getUser(socket.id);
+    userLeavesApp(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        buildMessage(ADMIN, `${user.name} has left`)
+      );
+
+      io.to(user.room).emit("userList", {
+        users: getUsersInRoom(user.room),
+      });
+
+      io.emit("roomList", {
+        rooms: getAllActiveRooms(),
+      });
+    }
+  });
+
+  //listen for a message
+  socket.on("message", ({ name, text }) => {
+    const room = getUser(socket.id)?.room;
+    if (room) {
+      io.to(room).emit("message", buildMessage(name, text));
+      console.log(text);
+    }
+  });
+
+  //listen for activity
+  socket.on("activity", (name) => {
+    const room = getUser(socket.id)?.room;
+    if (room) {
+      socket.broadcast.to(room).emit("activity", name);
+    }
+  });
+});
+
+function buildMessage(name, text) {
+  return {
+    name,
+    text,
+    time: new Intl.DateTimeFormat("default", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    }).format(new Date()),
+  };
+}
+
+// User functions
+function activateUser(id, name, room) {
+  const user = { id, name, room };
+  UsersState.setUsers([
+    ...UsersState.users.filter((user) => user.id !== id),
+    user,
+  ]);
+  return user;
+}
+
+function userLeavesApp(id) {
+  UsersState.setUsers(UsersState.users.filter((user) => user.id !== id));
+}
+
+function getUser(id) {
+  return UsersState.users.find((user) => user.id === id);
+}
+
+function getUsersInRoom(room) {
+  return UsersState.users.filter((user) => user.room === room);
+}
+
+function getAllActiveRooms() {
+  return Array.from(new Set(UsersState.users.map((user) => user.room)));
+}
